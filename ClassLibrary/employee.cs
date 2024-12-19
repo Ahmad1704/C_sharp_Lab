5657add1be9a5d8bd2e66d65cf6367a8b5e04e5a
Ahmad1704 <ahmadsaeed1704@gmail.com>
2024-12-19 15:25:27 +0200

    Day02_Lab

---
+++ ClassLibrary/employee.cs
@@ -0,0 +1,86 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+
+namespace ClassLibrary
+{
+    public class employee : IComparable
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        private int age;
+
+        private int target;
+
+        private int salary;
+
+        public hiringDate HiringDate;
+
+        public securityLevel SecurityLevel;
+
+        public Gender Gender;
+
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 18 && value < 45)
+                    age = value;
+            }
+        }
+
+        public int Target
+        {
+            get { return target; }
+            set
+            {
+                if (value > 300)
+                    target = value;
+            }
+        }
+
+        public int Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value > 6000 && value < 20000)
+                    salary = value;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"Id :{Id}  Name: {Name} , age {age} ,target {target},salary {salary}, Gender {Gender},hiringDate :{HiringDate.Day}/{HiringDate.Month}/{HiringDate.Year},securityLevel {SecurityLevel} ";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            
+            employee? emp = obj as employee;
+            if (emp == null)
+                throw new ArgumentException("Object is not an employee");
+
+            if (HiringDate.Year > emp.HiringDate.Year)
+                return 1;
+            else if (HiringDate.Year < emp.HiringDate.Year)
+                return -1;
+
+            if (HiringDate.Month > emp.HiringDate.Month)
+                return 1;
+            else if (HiringDate.Month < emp.HiringDate.Month)
+                return -1;
+
+            if (HiringDate.Day > emp.HiringDate.Day)
+                return 1;
+            else if (HiringDate.Day < emp.HiringDate.Day)
+                return -1;
+
+            return 0; 
+        }
+    }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,88 @@
+﻿using ClassLibrary;
+using System.Reflection;
+
+namespace Day02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            employee[] employees = new employee[3];
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                employees[i] = new employee();
+                Console.WriteLine($"Please enter the employee id");
+                employees[i].Id = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Please enter the employee Name");
+                employees[i].Name = Console.ReadLine();
+                do
+                {
+                    Console.WriteLine($"Please enter the employee salary");
+                    employees[i].Salary = int.Parse(Console.ReadLine());
+                } while (!(employees[i].Salary < 6000 && employees[i].Salary > 20000));
+
+                do
+                {
+                    Console.WriteLine($"Please enter the employee Target");
+                    employees[i].Target = int.Parse(Console.ReadLine());
+                } while (!(employees[i].Target <300));
+
+                do
+                {
+                    Console.WriteLine($"Please enter the employee age");
+                    employees[i].Age = int.Parse(Console.ReadLine());
+                } while (!(employees[i].Age < 18 && employees[i].Salary < 45));
+
+                Console.WriteLine($"Please enter the Hiring  date");
+                Console.WriteLine($"day");
+                employees[i].HiringDate.Day = int.Parse(Console.ReadLine());
+                Console.WriteLine($"month");
+                employees[i].HiringDate.Month = int.Parse(Console.ReadLine());
+                Console.WriteLine($"year");
+                employees[i].HiringDate.Year = int.Parse(Console.ReadLine());
+                Console.WriteLine($"please enter the employee gender enter 1 for male / 2 for female");
+                int selectedNumber = int.Parse(Console.ReadLine());
+                switch (selectedNumber)
+                {
+                    case 1:
+                        employees[i].Gender = Gender.Male; break;
+                    case 2:
+                        employees[i].Gender = Gender.Female; break;
+                    default:
+                        break;
+                }
+                Console.WriteLine($"please enter the employee Security Level enter 1 for admin / 2 for hr, /3 for officer");
+                int selectedNumber1 = int.Parse(Console.ReadLine());
+                switch (selectedNumber1)
+                {
+                    case 1:
+                        employees[i].SecurityLevel = securityLevel.admin; break;
+                    case 2:
+                        employees[i].SecurityLevel = securityLevel.hr; break;
+                    case 3:
+                        employees[i].SecurityLevel = securityLevel.officer; break;
+                    default:
+                        break;
+                }
+
+            }
+
+            Console.WriteLine("before");
+            foreach(employee emp in employees)
+            {
+                Console.WriteLine(emp);
+            }
+            Console.WriteLine("after");
+
+            Array.Sort(employees);
+
+            foreach (employee emp in employees)
+            {
+                Console.WriteLine(emp);
+            }
+        }
+
+
+    }
+}
