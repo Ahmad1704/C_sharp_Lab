3b4c341c0c3cda7db6d0dd32c0c19139e2e2da76
Ahmad1704 <101527171+Ahmad1704@users.noreply.github.com>
2024-12-25 15:39:44 +0200

    day04

---
+++ Book.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day04
+{
+
+    public delegate T GeneralDel<in T1, out T>(T1 x);
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+
+        public Book(string _ISBN, string _Title, string[] _Authors, DateTime _PublicationDate, decimal _Price)
+        {
+            ISBN = _ISBN;
+            Title = _Title;
+            Authors = _Authors;
+            PublicationDate = _PublicationDate;
+            Price = _Price;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Title: {Title}, ISBN: {ISBN}, Authors: {string.Join(", ", Authors)}, Publication Date: {PublicationDate.ToShortDateString()}, Price: {Price:C}";
+        }
+    }
+
+    public class BookFunctions
+    {
+
+        public static string GetTitle(Book B)
+        {
+            return B.Title;
+        }
+
+
+        public static string GetAuthors(Book B)
+        {
+            return string.Join(", ", B.Authors);
+        }
+
+
+        public static string GetPrice(Book B)
+        {
+            return B.Price.ToString();
+        }
+    }
+
+    public class LibraryEngine
+    {
+
+        public static void ProcessBooks(List<Book> bList, GeneralDel<Book, string> del)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(del(B));
+            }
+        }
+        public static void ProcessBooks(List<Book> bList, Func<Book, string> fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Day04;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Book> books = new List<Book>
+        {
+            new Book("978-3-16-148410-0", "Book A", new[] { "Author A", "Author B" }, new DateTime(2020, 1, 1), 29.99m),
+            new Book("978-1-23-456789-0", "Book B", new[] { "Author C" }, new DateTime(2021, 5, 15), 19.99m)
+        };
+
+        GeneralDel<Book,string> del;
+        del  = BookFunctions.GetTitle;
+
+       
+        Func<Book, string> fPtrBcl = BookFunctions.GetAuthors;
+
+    
+        Func<Book, string> fPtrAnonymous = delegate (Book B)
+        {
+            return B.ISBN;
+        };
+
+       
+        Func<Book, string> fPtrLambda = (Book B) => B.PublicationDate.ToShortDateString();
+
+      
+        Console.WriteLine("Get Title:");
+        LibraryEngine.ProcessBooks(books, del);
+
+        Console.WriteLine("\nGet Authors (BCL):");
+        LibraryEngine.ProcessBooks(books, fPtrBcl);
+
+        Console.WriteLine("\nGet ISBN (Anonymous Method):");
+        LibraryEngine.ProcessBooks(books, fPtrAnonymous);
+
+        Console.WriteLine("\nGet Publication Date (Lambda Expression):");
+        LibraryEngine.ProcessBooks(books, fPtrLambda);
+    }
+}
